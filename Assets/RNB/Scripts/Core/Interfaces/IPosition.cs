eb1d8df580f7fa678e457447422b659dcd140401
Rohith Nanthan <b.rohithnanthan@gmail.com>
2024-05-18 17:41:45 +0530

    Created Raycaster

---
+++ Assets/RNB/Scripts/Core/Interfaces/IPosition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RNB.Core.Interfaces
+{
+    public interface IPosition
+    {
+        public Vector2 Position { get; }
+    }
+}
---
+++ Assets/RNB/Scripts/Detection/Raycaster.cs
@@ -0,0 +1,60 @@
+using RNB.Core;
+using RNB.Core.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RNB.Detection
+{
+    public class Raycaster : MonoBehaviour, IHitDetector
+    {
+        private IDirection _raycastDirection;
+        [SerializeField] private MonoBehaviour _raycastDirectionComponent;
+
+        private IPosition _startingPosition;
+        [SerializeField] private MonoBehaviour _startingPositionComponent;
+
+        [SerializeField] private float _raycastDistance;
+        [SerializeField] private LayerMask _layerMask;
+
+        #region IHitDetector
+        public RaycastHit2D LastHitInfo { get; private set; }
+
+        private bool _isHit = false;
+        public bool IsHit
+        {
+            get => _isHit;
+
+            set
+            {
+                if (_isHit == value)
+                {
+                    return;
+                }
+
+                _isHit = value;
+                OnHitStatusChange?.Invoke(value);
+            }
+        }
+
+        public event Action<bool> OnHitStatusChange;
+        #endregion
+
+        private void Awake()
+        {
+            _raycastDirection = _raycastDirectionComponent as IDirection;
+            _startingPosition = _startingPositionComponent as IPosition;
+        }
+
+        private void FixedUpdate()
+        {
+            LastHitInfo = Physics2D.Raycast(origin: _startingPosition.Position, 
+                direction: _raycastDirection.Direction, 
+                distance: _raycastDistance, 
+                layerMask: _layerMask);
+
+            IsHit = LastHitInfo.collider != null;
+        }
+    }
+}
