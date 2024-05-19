22942bc022f4add317c166e37f20d69612d82307
Rohith Nanthan <b.rohithnanthan@gmail.com>
2024-05-19 13:25:55 +0530

    Created scriptable object to tune all player controls

---
+++ Assets/RNB/Scripts/Player/PlayerControlScriptableObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RNB.Player
+{
+    [CreateAssetMenu(fileName = "NewPlayerControl", menuName = "RNB/PlayerScriptable/PlayerControl", order = 1)]
+    public class PlayerControlScriptableObject : ScriptableObject
+    {
+        public float _MovementSpeedInGround = 5f;
+        public float _MovementSpeedInAir = 5f;
+
+        [Header("Falling Down")]
+        public float _InitialFallingDownSpeed = 5f;
+        public float _FallingDownAccleration = 9.8f;
+        public float _MaxFallingDownSpeed = 30f;
+
+        [Header("Jump")]
+        public float _InitialJumpSpeed = 8f;
+        public float _JumpSpeedDeaccleration = 9.8f;
+    }
+}
