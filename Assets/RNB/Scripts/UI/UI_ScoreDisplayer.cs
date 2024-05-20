e033454f5d58820612824dd399619c31364c103d
Rohith Nanthan <b.rohithnanthan@gmail.com>
2024-05-20 23:21:17 +0530

    Integrated score to UI

---
+++ Assets/RNB/Scripts/UI/UI_ScoreDisplayer.cs
@@ -0,0 +1,36 @@
+using RNB.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace RNB.UI
+{
+    public class UI_ScoreDisplayer : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _scoreText;
+        [SerializeField] private InventoryScriptableBase _inventory;
+
+        private const string SCORE_TEXT = "Score";
+
+        private void Start()
+        {
+            _scoreText.text = $"{SCORE_TEXT}: {_inventory.CurrentScore}";
+        }
+
+        private void OnEnable()
+        {
+            _inventory.OnScoreChange += UpdateScoreText;
+        }
+
+        private void OnDisable()
+        {
+            _inventory.OnScoreChange -= UpdateScoreText;
+        }
+
+        private void UpdateScoreText(int previousScore, int currentScore)
+        {
+            _scoreText.text = $"{SCORE_TEXT}: {currentScore}";
+        }
+    }
+}
