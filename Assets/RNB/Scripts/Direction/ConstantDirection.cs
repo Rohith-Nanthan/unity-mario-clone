5e77b2e0570eb405d09b9e8573192a7c1f81861b
Rohith Nanthan <b.rohithnanthan@gmail.com>
2024-05-23 20:19:37 +0530

    Added comments for detection and direction

--- Assets/RNB/Scripts/Direction/ConstantDirection.cs
+++ Assets/RNB/Scripts/Direction/ConstantDirection.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 namespace RNB.Direction
 {
+    /// <summary>
+    /// Holds a Vector2 direction that never changes at run-time
+    /// <br/>Author: rohith.nanthan
+    /// </summary>
     public class ConstantDirection : MonoBehaviour, IDirection
     {
         [field:SerializeField] public Vector2 Direction{ get; private set; }
